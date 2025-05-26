10751c499fbba372c8c14b14057e1df1ca1c4ed9
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-05-26 20:56:38 +0700

    menambahkan distruktor

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -11,6 +11,10 @@ public:
     {
         cout << "Ibu \"" << nama << "\" ada\n";
     }
-    
+    ~ibu()
+    {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak *);
+    void cetakAnak();
 };
-#endif
