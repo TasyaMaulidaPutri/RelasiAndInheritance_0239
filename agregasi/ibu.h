880845b67b61b84f20b1dc2e441d8657e51c9d83
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-05-26 20:55:45 +0700

    menambahkan access modifire

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -2,3 +2,15 @@
 #define IBU_H
 
 class ibu
+{
+public:
+    string nama;
+    vector<anak *> daftar_anak;
+
+    ibu(string pNama) : nama(pNama)
+    {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    
+};
+#endif
